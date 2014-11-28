c25cc84b1703f3eb335da0ade43d6413a05f7eac
Florian THIERRY <florian.thierry@bts-malraux.net>
2014-11-28 08:38:29 +0100

    La comboboxx est alimentée dynamiquement en fonction de quel menuitem on a cliqué

--- src/vues/JPanelEleves.java
+++ src/vues/JPanelEleves.java
@@ -1,88 +1,240 @@
 package vues;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
+import controleurs.ControleurPrincipal;
+import controleurs.ErrorManagement;
+import controleurs.RegexTests;
+import classes.Division;
+import classes.Eleve;
+import dao.EleveDAO;
+import java.awt.Color;
+import java.awt.Font;
 
 public class JPanelEleves extends JPanel {
-	private int codeDivision;
-	private JTextField txtNom;
-	private JTextField txtPrenom;
-	private JTextField txtDateNaiss;
+	public JTextField txbNom;
+	public JTextField txbPrenom;
+	public JTextField txbDateNaiss;
+	public JComboBox<String> cbbEleve;
+	public JButton btnAjouter;
+	public JLabel lblError;
+	public JLabel lblClasse;
+	public JButton btnValider;
+	public JButton btnModifier;
+	public JButton btnSupprimer;
+	public Division laDivision;
+	private JSeparator separator;
+	private JLabel lblNom;
+	private JLabel lblPrenom;
+	private JLabel lblDateNaiss;
+	private JSeparator separator2;
+	public List<Eleve> listeEleves;
+	
 	
 	/**
 	 * Constructor of JPanelEleves
 	 */
-	public JPanelEleves(int codeDivision) {
-		super();
+	public JPanelEleves(ControleurPrincipal controlleur) {
+		listeEleves = new ArrayList<Eleve>();
 		//One variable
-		this.codeDivision = codeDivision;
 		setLayout(null);
 		//Panel label
-		JLabel lblClasse = new JLabel("Classe de ");
-		lblClasse.setBounds(40, 35, 117, 24);
-		add(lblClasse);
+		JLabel lblTitle = new JLabel("Classe de ");
+		lblTitle.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblTitle.setBounds(209, 25, 117, 24);
+		add(lblTitle);
 		//Button ajouter
-		JButton btnAjouter = new JButton("Ajouter");
-		btnAjouter.setBounds(40, 70, 86, 24);
+		btnAjouter = new JButton("Ajouter");
+		btnAjouter.addActionListener(controlleur);
+		
+		lblClasse = new JLabel("SLAM");
+		lblClasse.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblClasse.setBounds(315, 30, 170, 15);
+		add(lblClasse);
+		btnAjouter.setBounds(65, 70, 86, 24);
 		add(btnAjouter);
 		//Label between the button and the combobox
 		JLabel lblSelect = new JLabel("ou sélectionner un nouvel élève :");
-		lblSelect.setBounds(145, 75, 276, 15);
+		lblSelect.setBounds(170, 75, 276, 15);
 		add(lblSelect);
-		
-		JComboBox cbbEleve = new JComboBox();
-		cbbEleve.setBounds(405, 75, 126, 24);
+		//The comboBox which contains the Division Eleves
+		cbbEleve = new JComboBox<String>();
+		cbbEleve.setBounds(439, 70, 190, 24);
+		cbbEleve.addActionListener(controlleur);
 		add(cbbEleve);
-		
-		JSeparator separator = new JSeparator();
-		separator.setBounds(90, 120, 430, 2);
+		//First separator
+		separator = new JSeparator();
+		separator.setBounds(137, 121, 430, 2);
+		separator.setVisible(false);
 		add(separator);
-		
-		JLabel lblNom = new JLabel("Nom :");
+		//Label "Nom"
+		lblNom = new JLabel("Nom :");
 		lblNom.setBounds(60, 150, 70, 15);
+		lblNom.setVisible(false);
 		add(lblNom);
-		
-		txtNom = new JTextField();
-		txtNom.setBounds(120, 150, 114, 19);
-		add(txtNom);
-		txtNom.setColumns(10);
-		
-		JLabel lblPrenom = new JLabel("Prénom :");
+		//Text bar "Nom"
+		txbNom = new JTextField();
+		txbNom.setName("Nom");
+		txbNom.setBounds(120, 150, 114, 19);
+		txbNom.setVisible(false);
+		add(txbNom);
+		txbNom.setColumns(10);
+		//Label "Prenom"
+		lblPrenom = new JLabel("Prénom :");
 		lblPrenom.setBounds(280, 150, 70, 15);
+		lblPrenom.setVisible(false);
 		add(lblPrenom);
 		
-		txtPrenom = new JTextField();
-		txtPrenom.setBounds(360, 150, 114, 19);
-		add(txtPrenom);
-		txtPrenom.setColumns(10);
+		txbPrenom = new JTextField();
+		txbPrenom.setName("Prénom");
+		txbPrenom.setBounds(360, 150, 114, 19);
+		txbPrenom.setVisible(false);
+		add(txbPrenom);
+		txbPrenom.setColumns(10);
 		
-		JLabel lblDateNaiss = new JLabel("Date de naissance :");
+		lblDateNaiss = new JLabel("Date de naissance :");
 		lblDateNaiss.setBounds(100, 190, 141, 15);
+		lblDateNaiss.setVisible(false);
 		add(lblDateNaiss);
 		
-		txtDateNaiss = new JTextField();
-		txtDateNaiss.setBounds(260, 190, 148, 19);
-		add(txtDateNaiss);
-		txtDateNaiss.setColumns(10);
+		txbDateNaiss = new JTextField();
+		txbDateNaiss.setName("Date de naissance");
+		txbDateNaiss.setBounds(260, 190, 148, 19);
+		txbDateNaiss.setVisible(false);
+		add(txbDateNaiss);
+		txbDateNaiss.setColumns(10);
 		
-		JSeparator separator2 = new JSeparator();
-		separator2.setBounds(90, 240, 430, 2);
+		separator2 = new JSeparator();
+		separator2.setBounds(137, 247, 430, 2);
+		separator2.setVisible(false);
 		add(separator2);
 		
-		JButton btnValider = new JButton("Valider");
-		btnValider.setBounds(100, 261, 96, 25);
+		btnValider = new JButton("Valider");
+		btnValider.addActionListener(controlleur);
+		btnValider.setBounds(150, 296, 96, 25);
+		btnValider.setVisible(false);
 		add(btnValider);
 		
-		JButton btnModifier = new JButton("Modifier");
-		btnModifier.setBounds(60, 261, 117, 25);
+		btnModifier = new JButton("Modifier");
+		btnModifier.setBounds(140, 296, 117, 25);
+		btnModifier.setVisible(false);
 		add(btnModifier);
 		
-		JButton btnSupprimer = new JButton("Supprimer");
-		btnSupprimer.setBounds(220, 261, 117, 25);
+		btnSupprimer = new JButton("Supprimer");
+		btnSupprimer.setBounds(460, 296, 117, 25);
+		btnSupprimer.setVisible(false);
 		add(btnSupprimer);
+		
+		lblError = new JLabel("Error :");
+		lblError.setForeground(Color.RED);
+		lblError.setBounds(32, 351, 648, 47);
+		lblError.setVisible(false);
+		add(lblError);
 	}//end JPanelEleves()
+	
+	/**
+	 * Initialize the Combobox with the Division Eleves
+	 * @param codeDivision
+	 */
+	public void initComboBox()
+	{
+		lblClasse.setText(laDivision.getLibelle());
+		showAddingPart(false);
+		cbbEleve.removeAllItems();
+		cbbEleve.addItem("");
+		listeEleves = new ArrayList<Eleve>(new EleveDAO().selectAllOfOneDivision(laDivision.getCode()));
+		for (Eleve OneEleve : listeEleves)
+		{
+			cbbEleve.addItem(OneEleve.getNom() +" "+ OneEleve.getPrenom());
+		}//end for
+	}//end initComboBox()
+	
+	/**
+	 * Show or hide the Adding-Eleve elements of the JPanelEleve
+	 * @param True : It shows these part ; False : It hides these part
+	 */
+	public void showAddingPart(boolean value)
+	{
+		if(value == true)
+		{
+			//We show the labels and the separators
+			this.separator.setVisible(value);
+			this.separator2.setVisible(value);
+			this.lblNom.setVisible(value);
+			this.lblPrenom.setVisible(value);
+			this.lblDateNaiss.setVisible(value);
+			//We show the textBars
+			this.txbNom.setVisible(value);
+			this.txbNom.requestFocus();
+			this.txbPrenom.setVisible(value);
+			this.txbDateNaiss.setVisible(value);
+			//We show the "Valider" button
+			this.btnValider.setVisible(value);
+			this.cbbEleve.setEnabled(!value);
+		}//end if
+		else
+		{
+			//We hide the labels and the separators
+			this.separator.setVisible(value);
+			this.separator2.setVisible(value);
+			this.lblNom.setVisible(value);
+			this.lblPrenom.setVisible(value);
+			this.lblDateNaiss.setVisible(value);
+			//We hide the textBars
+			this.txbNom.setVisible(value);
+			this.txbPrenom.setVisible(value);
+			this.txbDateNaiss.setVisible(value);
+			//We hide the "Valider" button
+			this.btnValider.setVisible(value);
+			this.cbbEleve.setEnabled(!value);
+		}//end else
+	}//end showAddingPart()
+	
+	/**
+	 * Carry out the seizure controls of text field before the new Eleve insertion into database
+	 * @param lesEleves
+	 * @throws Exception
+	 */
+	public void addANewEleve() throws Exception
+	{
+		ErrorManagement.checkEmptyField(txbNom);
+		//if the name seizure is wrong, an exception is thrown
+		if(RegexTests.isOneWord(txbNom.getText()) == false)
+		{
+			throw new Exception("\"" + txbNom.getText() + "\" is not a valid name !<br />Ensure that it don't contain numbers or punctuation characters", new Throwable("format"));
+		}//end if
+		
+		ErrorManagement.checkEmptyField(txbPrenom);
+		//if the first name seizure is wrong, an exception is thrown
+		if (RegexTests.isOneWord(txbPrenom.getText()) == false)
+		{
+			throw new Exception("\"" + txbNom.getText() + "\" is not a valid first name !<br />Ensure that it don't contain numbers or punctuation characters", new Throwable("format"));
+		}//end else if
+		
+		ErrorManagement.checkEmptyField(txbDateNaiss);
+		//if the date seizure is wrong, an exception is thrown
+		if(RegexTests.isOneDate(txbDateNaiss.getText()) == false)
+		{
+			throw new Exception("\"" + txbDateNaiss.getText() + "\" is not a valid date !<br />Ensure you wrote a date in this format : \"XXXX-XX-XX\"", new Throwable("date"));
+		}//end else if
+		
+		EleveDAO lesEleves = new EleveDAO();
+		//if there're no wrong seizures, a new Eleve is created into the database
+		lesEleves.create(new Eleve(lesEleves.selectMaxCodeFromEleves(),
+									txbNom.getText(),
+									txbPrenom.getText(),
+									txbDateNaiss.getText(),
+									laDivision));
+	}//end addANewEleve
+	
+	public void showUpdatingPart(boolean value)
+	{
+		
+	}
 }//end Class()
