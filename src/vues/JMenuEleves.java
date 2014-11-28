362d240a34bc941bd5153ec304820c21d27d8abc
Florian THIERRY <florian.thierry@bts-malraux.net>
2014-11-28 08:37:38 +0100

    Création des MenuItems du Menu Eleve de façon dynamique avec la bdd

--- src/vues/JMenuEleves.java
+++ src/vues/JMenuEleves.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-import dao.DivisionDAO;
-import dao.DAO;
 import classes.Division;
+import controleurs.ControleurPrincipal;
+import dao.DAO;
+import dao.DivisionDAO;
 
 public class JMenuEleves extends JMenu {
-	public JMenuEleves(String libelle)
+	public JMenuEleves(String libelle, ControleurPrincipal controlleur)
 	{
 		super(libelle);
 		DAO<Division> theDivisions = new DivisionDAO();
@@ -18,6 +19,7 @@ public class JMenuEleves extends JMenu {
 		for(Division oneDivision : divisionList)
 		{
 			JMenuItem newMenuItem = new JMenuItem(oneDivision.getLibelle());
+			newMenuItem.addActionListener(controlleur);
 			newMenuItem.setActionCommand(Integer.toString(oneDivision.getCode()));
 			this.add(newMenuItem);
 		}//enf foreach()
--- src/vues/MainFrame.java
+++ src/vues/MainFrame.java
@@ -1,52 +1,33 @@
 package vues;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
-
-import classes.Division;
-import dao.ConnexionPostgreSQL;
-import dao.DAO;
-import dao.DivisionDAO;
-
+import controleurs.ControleurPrincipal;
+import java.awt.Rectangle;
 
 public class MainFrame extends JFrame {
-
-	private JPanel contentPane;
-	private JPanelEleves panelEleve;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					MainFrame frame = new MainFrame();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	public ControleurPrincipal controlleur;
+	public JPanel contentPane;
+	public JPanelEleves panelEleve;
+	public JMenuItem mntmQuitter;
 
 	/**
 	 * Create the frame.
 	 */
-	public MainFrame() {
+	public MainFrame(ControleurPrincipal controlleur) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 720, 491);
-		
+		this.controlleur = controlleur;
+		contentPane = new JPanel();
+		contentPane.setBounds(new Rectangle(0, 0, 238, 150));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
 		//Addition of the MenuBar
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
@@ -54,24 +35,17 @@ public class MainFrame extends JFrame {
 		JMenu mnDivision = new JMenu("Division");
 		menuBar.add(mnDivision);
 		//Addition of the JMenuEleves named "Élèves" to the JManuBar
-		JMenuEleves mnEleves = new JMenuEleves("Élèves");
+		JMenuEleves mnEleves = new JMenuEleves("Élèves", this.controlleur);
 		menuBar.add(mnEleves);
+		//Addition of the JPanelEleve
+		panelEleve = new JPanelEleves(this.controlleur);
+		panelEleve.setVisible(false);
+		contentPane.add(panelEleve);
 		
-		JMenuItem mntmQuitter = new JMenuItem("Quitter");
+		mntmQuitter = new JMenuItem("Quitter");
+		mntmQuitter.addActionListener(this.controlleur);
 		menuBar.add(mntmQuitter);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		
+		this.setVisible(true);
 	}//end Constructor
-	
-	private void initMenuItemEleve()
-	{
-		//récupérer toutes les divisions
-		
-		//pour chaque division, on va créér un JPanelEleve, on renseignera le text avec le libelle de la division, et le nom avec le code
-		
-		//on ajoute chaque nouveau JPanelEleve au menuitem mnEleves
-	}
-
-}
+}//end Class
